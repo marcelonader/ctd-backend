ea6ae207827d079529f2a1faa0dfcdb8b00656cb
marcelonader <marcelo.m.nader@gmail.com>
2021-11-11 21:14:51 -0300

    desafios realizados na aula de backend do curso CTD

---
+++ maven-spring-boot-aula-09.11.2021 - Copia/Trainer/src/main/java/com/example/Trainer/service/impl/TrainerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.Trainer.service.impl;
+
+import com.example.Trainer.model.Trainer;
+import com.example.Trainer.service.TrainerService;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TrainerServiceImpl implements TrainerService {
+    @Override
+    public List<Trainer> listTrainer() {
+        return Arrays.asList(new Trainer("Joana"), new Trainer("Rafael"), new Trainer("Josilvaldo"));
+    }
+}
